fe0836489710c1835e8f88a470a285427a6f8cda
SurabhiKC <128141142+Surabhi-KC@users.noreply.github.com>
2025-04-15 11:05:56 +0530

    Delete src/main directory

--- src/main/java/com/supermarketims/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.supermarketims.repository;
-
-import com.supermarketims.model.OrderItem;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    List<OrderItem> findByOrderId(Long orderId);
-}
